4f6ac88170abb6a01868a45c59d2118a4ab13a69
minhhy2801 <minhhy2897@gmail.com>
2019-11-01 14:30:54 +0700

    build dist

---
+++ dist/migrate/migrateAppBasedOnConfig.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const service = require('../util/service');
+
+const config = require('../util/config');
+
+const util = require('../util/util');
+
+const migrateNewApp = require('./migrateNewApp');
+
+const clc = require("cli-color");
+
+const kintoneAppCopy = service.getKintoneAuth(config.TARGET.DOMAIN, config.TARGET.USERNAME, config.TARGET.PASSWORD);
+const kintoneAppPaste = service.getKintoneAuth(config.MIGRATE.DOMAIN, config.MIGRATE.USERNAME, config.MIGRATE.PASSWORD);
+const appCopy = config.TARGET.APP_ID;
+const appPaste = config.MIGRATE.APP_ID;
+
+const createListApps = async listAppIds => {
+  try {
+    listAppIds = [...new Set(listAppIds)];
+    let listCreatedApp = {};
+
+    for (let i = 0; i < listAppIds.length; i++) {
+      listCreatedApp[listAppIds[i]] = await migrateNewApp.run(listAppIds[i], config.TARGET.DOMAIN, config.TARGET.USERNAME, config.TARGET.PASSWORD, config.MIGRATE.DOMAIN, config.MIGRATE.USERNAME, config.MIGRATE.PASSWORD);
+    }
+
+    return listCreatedApp;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const run = async () => {
+  try {
+    let fieldsCopy = await service.getAllFormFields(appCopy, kintoneAppCopy);
+    let listAppIds = util.getAppIdsOfRelatedRecordLookup(fieldsCopy, appCopy);
+    let listNewAppIds = await createListApps(listAppIds);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    fieldsCopy = util.setFieldsCopyRelatedRecordLookup(fieldsCopy, listNewAppIds, appCopy, appPaste);
+    let fieldsPaste = await service.getAllFormFields(appPaste, kintoneAppPaste);
+    const updateFields = util.getAllSpecialCodeFieldsForUpdate(fieldsPaste, fieldsCopy);
+    const updateSpecialFields = await service.updateSpecialFields(appPaste, updateFields, kintoneAppPaste);
+    await service.deployApp(appPaste, updateSpecialFields.revision, kintoneAppPaste);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    const fieldsForAdd = util.getAllFormFieldsForAdd(fieldsCopy, fieldsPaste);
+    const resultAddFields = await service.addAllFormFields(appPaste, fieldsForAdd, kintoneAppPaste);
+    await service.deployApp(appPaste, resultAddFields.revision, kintoneAppPaste);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    const layouts = await service.getFormLayoutApp(appCopy, kintoneAppCopy);
+    const updateLayout = await service.updateFormLayout(appPaste, layouts, kintoneAppPaste);
+
+    if (updateLayout.hasOwnProperty('revision')) {
+      service.deployApp(appPaste, updateLayout.revision, kintoneAppPaste).then(rsp => {
+        console.log(clc.green(`Migrate app id ${appPaste} successfully!`));
+      }).catch(err => {
+        console.log(clc.red(`Deploy app id ${appPaste} fail!`));
+      });
+    } else {
+      console.log(clc.red(`Migrate app id ${appPaste} fail!`));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  run
+};
---
+++ dist/util/service.js
@@ -0,0 +1,131 @@
+"use strict";
+
+const kintone = require('@kintone/kintone-js-sdk');
+
+const getKintoneAuth = (domain, username, password) => {
+  let auth = new kintone.Auth();
+  auth.setPasswordAuth({
+    username,
+    password
+  });
+  const connection = new kintone.Connection({
+    domain,
+    auth
+  });
+  const kintoneApp = new kintone.App({
+    connection
+  });
+  return kintoneApp;
+};
+
+const getAllFormFields = (appId, kintoneApp) => {
+  return kintoneApp.getFormFields({
+    app: appId
+  }).then(rsp => {
+    return rsp.properties;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const addAllFormFields = (app, fields, kintoneApp) => {
+  return kintoneApp.addFormFields({
+    app,
+    fields
+  }).then(rsp => {
+    return rsp;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const updateSpecialFields = (app, fields, kintoneApp) => {
+  return kintoneApp.updateFormFields({
+    app,
+    fields
+  }).then(rsp => {
+    return rsp;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const deployApp = (app, revision, kintoneApp) => {
+  const apps = [{
+    app,
+    revision
+  }];
+  const revert = false;
+  return kintoneApp.deployAppSettings({
+    apps,
+    revert
+  }).then(rsp => {
+    return rsp;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const createNewApp = (name, kintoneApp) => {
+  return kintoneApp.addPreviewApp({
+    name
+  }).then(rsp => {
+    return rsp;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const getDeployStatus = (apps, kintoneApp) => {
+  return kintoneApp.getAppDeployStatus({
+    apps
+  }).then(rsp => {
+    return rsp.apps;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const getInformationApp = (id, kintoneApp) => {
+  return kintoneApp.getApp({
+    id
+  }).then(rsp => {
+    return rsp;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const updateFormLayout = (app, layout, kintoneApp) => {
+  return kintoneApp.updateFormLayout({
+    app,
+    layout
+  }).then(rsp => {
+    return rsp;
+  }).catch(err => {
+    return err;
+  });
+};
+
+const getFormLayoutApp = (app, kintoneApp) => {
+  return kintoneApp.getFormLayout({
+    app
+  }).then(rsp => {
+    return rsp.layout;
+  }).catch(err => {
+    return err;
+  });
+};
+
+module.exports = {
+  getKintoneAuth,
+  getFormLayoutApp,
+  updateFormLayout,
+  getInformationApp,
+  getDeployStatus,
+  createNewApp,
+  deployApp,
+  updateSpecialFields,
+  addAllFormFields,
+  getAllFormFields
+};
